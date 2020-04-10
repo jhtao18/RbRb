af461474e63dedb4aefba8d908113bdb7304eadd
Junheng <jhtao18@umd.edu>
2020-04-09 20:38:09 -0400

    try

--- BEC.py
+++ BEC.py
@@ -1,7 +1,14 @@
+from labscript import *
+from labscriptlib.common.utils import Limits
+from labscriptlib.common.functions import *
+from labscript_utils import import_or_reload
+import_or_reload('labscriptlib.RbRb.connectiontable')
+
 def prep(t):
     print("hei")
 
 start()
+print(aa)
 t = 0
 t += 1
 stop(t)
